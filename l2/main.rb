8bccec089356334febd9a57c375c84d8f99f1fb2
MeowningMaster <meowningmaster@gmail.com>
2022-09-10 16:49:36 +0300

    lab2

---
+++ l2/main.rb
@@ -0,0 +1,19 @@
+# variant 10
+
+require_relative './parser'
+
+def sumPart(i1, i2)
+  p1, p2 = [POINTS[i1], POINTS[i2]]
+  puts "#{i1} #{i2}"
+  xs = p1[:x] + p2[:x]
+  yd = p1[:y] - p2[:y]
+  (xs*yd)
+end
+
+last_index = POINTS.length - 1
+area = (0...last_index)
+         .map { |i| sumPart(i, i + 1) }
+         .push(sumPart(last_index, 0))
+         .sum.abs.to_f / 2
+
+puts area
